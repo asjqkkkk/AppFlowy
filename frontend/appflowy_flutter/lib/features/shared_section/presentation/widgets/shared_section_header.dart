786e6a599e64cc4aedd2f59e0e9b61d70fb7d334
Lucas.Xu <lucas.xu@appflowy.io>
2025-06-26 14:41:55 +0800

    feat: add expand icon for fav header and shared header

--- frontend/appflowy_flutter/lib/features/shared_section/presentation/widgets/shared_section_header.dart
+++ frontend/appflowy_flutter/lib/features/shared_section/presentation/widgets/shared_section_header.dart
@@ -8,29 +8,62 @@ class SharedSectionHeader extends StatelessWidget {
   const SharedSectionHeader({
     super.key,
     required this.onTap,
+    this.isExpanded = false,
   });
 
   final VoidCallback onTap;
+  final bool isExpanded;
 
   @override
   Widget build(BuildContext context) {
     final theme = AppFlowyTheme.of(context);
-    return AFGhostIconTextButton.primary(
-      text: LocaleKeys.shareSection_shared.tr(),
-      mainAxisAlignment: MainAxisAlignment.start,
-      size: AFButtonSize.l,
-      onTap: onTap,
-      // todo: ask the designer to provide the token.
+    return AFBaseButton(
+      backgroundColor: (context, isHovering, disabled) {
+        final theme = AppFlowyTheme.of(context);
+        if (disabled) {
+          return Colors.transparent;
+        }
+        if (isHovering) {
+          return theme.fillColorScheme.contentHover;
+        }
+        return Colors.transparent;
+      },
+      borderColor: (context, isHovering, disabled, isFocused) {
+        return Colors.transparent;
+      },
       padding: EdgeInsets.only(
         left: 6,
         top: 6,
         bottom: 6,
       ),
       borderRadius: theme.borderRadius.s,
-      iconBuilder: (context, isHover, disabled) => FlowySvg(
-        FlowySvgs.shared_with_me_m,
-        color: theme.badgeColorScheme.color13Thick2,
-      ),
+      onTap: onTap,
+      builder: (context, isHovering, disabled) {
+        final textColor = theme.textColorScheme.primary;
+        return Row(
+          children: [
+            FlowySvg(
+              FlowySvgs.shared_with_me_m,
+              color: theme.badgeColorScheme.color13Thick2,
+            ),
+            SizedBox(width: theme.spacing.s),
+            Text(
+              LocaleKeys.shareSection_shared.tr(),
+              style: AFButtonSize.l.buildTextStyle(context).copyWith(
+                    color: textColor,
+                  ),
+            ),
+            SizedBox(width: theme.spacing.xs),
+            FlowySvg(
+              isExpanded
+                  ? FlowySvgs.workspace_drop_down_menu_show_s
+                  : FlowySvgs.workspace_drop_down_menu_hide_s,
+              color:
+                  isHovering ? Theme.of(context).colorScheme.onSurface : null,
+            ),
+          ],
+        );
+      },
     );
   }
 }
